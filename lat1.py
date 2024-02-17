0080aca81a4110e8dc251909e7dbbaf86b3b1e73
divino <divinoeldadsonkombong@gmail.com>
2024-02-17 16:56:59 +0900

    v3

--- lat1.py
+++ lat1.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
-st.write("Coding Club 2024")
-import matplotlib.pyplot as plt
+st.write(" Infinity Coding Club 2024")
+#import matplotlib.pyplot as plt
 
 df = pd.DataFrame ({
     'No': [1, 2, 3, 4],
     'Nama' : ['Nurul','Nadia','Vino','Zahwa'],
-    'Nilai' : [98, 90, 83, 92]
+    'Nilai' : [98, 90, 100, 92]
 })
 
 df
