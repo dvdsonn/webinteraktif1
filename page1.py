a42e0f8187d82b4c26926f58a71a85b1cfdafd50
divino <divinoeldadsonkombong@gmail.com>
2024-03-02 13:08:43 +0900

    v4

--- page1.py
+++ page1.py
@@ -1,4 +1,6 @@
 import streamlit as st
 def page_1():
-    st.title("Halaman 1")
+    st.title("PENGANTAR")
     st.write('Halaman ini digunakan untuk Intro')
+    st.write('7 Tips Jadi Pro Player Esports')
+
--- page3.py
+++ page3.py
@@ -5,4 +5,4 @@ def page_3():
     
     with open ('untitled72.md' , 'r') as file:
         isi_teks = file.read()
-    st.markdown(isi_teks)    
+        st.markdown(isi_teks)    
