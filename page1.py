44cd882890ad0be0fb4898664bbf7da05238bb63
divino <divinoeldadsonkombong@gmail.com>
2024-03-02 13:32:24 +0900

    v8

--- page1.py
+++ page1.py
@@ -2,5 +2,20 @@ import streamlit as st
 def page_1():
     st.title("BELAJAR MENJADI PRO PLAYER")
     st.write('7 Tips Jadi Pro Player Esports Tips menjadi pro player')
-    st.write('fokus pada satu game, pelajari mekanika permainan, berlatih secara teratur, tonton pertandingan profesional, gunakan peralatan yang tepat, dan jaga kesehatan fisik dan mental.')
-
+   
+    st.write('1.fokus pada satu game,Dalam menjadi seorang pro player, fokus pada satu game adalah kunci sukses untuk mengasah kemampuan dan meningkatkan performa. Menghabiskan waktu untuk memahami setiap mekanisme dan nuansa dari satu game dapat membantu para pemain mengembangkan strategi yang matang dan meningkatkan gameplay mereka.')
+   
+    st.write('2.Pelajari Mekanika Permainan,Ketika kamu ingin menjadi pro player, penting untuk mempelajari mekanika permainan dengan baik. Jangan hanya sekedar bermain asal-asalan, tapi cobalah memahami setiap detail dari game yang kamu mainkan. Pelajari bagaimana permainan bekerja, seperti item, karakter, dan strategi. Dengan memahami mekanika permainan, kamu bisa menemukan kelemahan dan kekuatan dari karakter dan item yang kamu gunakan.')
+   
+    st.write('3.Berlatih Secara Teratur,Berlatih secara teratur adalah kunci dalam mencapai kesuksesan sebagai seorang pro player. Tidak hanya cukup bermain game secara casual, tapi kamu juga perlu mengambil waktu untuk berlatih dengan serius. Dengan terus berlatih, kamu akan dapat meningkatkan kemampuanmu, memperbaiki kesalahan, dan mengembangkan strategi yang lebih baik.')
+   
+    st.write('4.Tonton Pertandingan Profesional,Tonton pertandingan profesional dan pelajari dari pemain terbaik. Perhatikan bagaimana mereka bermain dan apa yang membuat mereka begitu hebat. Anda dapat belajar banyak dari teknik dan strategi mereka.')
+   
+    st.write('5.Berteman dengan Pemain Lain,Berteman dengan pemain lain dapat membantu Anda belajar lebih cepat dan menjadi lebih baik. Cobalah untuk bergabung dengan komunitas game dan bergaul dengan pemain yang memiliki keterampilan yang sama atau lebih baik dari Anda. Diskusikan strategi dan teknik dengan mereka.')
+   
+    st.write('6. Gunakan Peralatan yang Tepat,Peralatan yang tepat dapat membantu Anda bermain lebih baik. Cobalah untuk membeli mouse, keyboard, dan monitor yang tepat untuk permainan yang Anda mainkan.')
+   
+    st.write('7. Bersaing di Turnamen,Ikuti turnamen dan bersainglah di dalamnya. Ini akan membantu Anda meningkatkan keterampilan Anda dan memberikan pengalaman bermain di level yang lebih tinggi.')
+   
+    st.write('Jadi, jangan lupa untuk berlatih secara teratur dan mempelajari mekanika permainan dengan baik jika kamu ingin menjadi seorang pro player. Tapi ingat, jangan terlalu serius dalam prosesnya, tetaplah bersenang-senang dan nikmati permainanmu. Siapa tahu, kamu bisa menjadi pro player berikutnya yang sukses di industri e-sports. Keep practicing and have fun!')
+    
