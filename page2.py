53c07e59c13464ad011d57cec8f09cd2c24969ac
divino <divinoeldadsonkombong@gmail.com>
2024-03-02 13:43:56 +0900

    v10

--- page2.py
+++ page2.py
@@ -2,5 +2,6 @@ import streamlit as st
 def page_2():
     st.title("TUTORIAL MENJADI PRO PLAYER")
     st.write('NONTON TU MICROBOY')
+    st.write('https://www.youtube.com/@Microboys/featured')
     st.video('https://www.youtube.com/watch?v=_JSKsEqWQTM&pp=ygUVcHViZyBtb2JpbGUgaW5kb25lc2lh')
     st.title('JANGAN LUPA SUBSCRIBE AGAR ANDA MENJADI PRO PLAYER')
