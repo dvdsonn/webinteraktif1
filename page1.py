179dac9e7c8d787c457ad15184613e0beaa7e2fd
divino <divinoeldadsonkombong@gmail.com>
2024-03-02 13:14:06 +0900

    v6

--- page1.py
+++ page1.py
@@ -2,5 +2,5 @@ import streamlit as st
 def page_1():
     st.title("PENGANTAR")
     st.write('Halaman ini digunakan untuk Intro')
-    st.write('7 Tips Jadi Pro Player Esports')
+    st.write('7 Tips Jadi Pro Player Esports Tips menjadi pro player: fokus pada satu game, pelajari mekanika permainan, berlatih secara teratur, tonton pertandingan profesional, gunakan peralatan yang tepat, dan jaga kesehatan fisik dan mental.')
 
