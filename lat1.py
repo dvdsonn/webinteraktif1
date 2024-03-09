24984408fb653c65f89e0bab83957dc30c5f7dcf
divino <divinoeldadsonkombong@gmail.com>
2024-03-09 11:25:43 +0900

    v15

--- lat1.py
+++ lat1.py
@@ -2,6 +2,7 @@ import streamlit as st
 from page1 import page_1
 from page2 import page_2
 from page3 import page_3
+from image1 import main
 from kalkulatorsegitiga import kalkulatorsegitiga
 # import pandas as pd
 # st.write(" Infinity Coding Club 2024")
@@ -21,7 +22,8 @@ PAGES = {
      "Page 1" : page_1,
      "Page 2" : page_2,
      "Page 3" : page_3,
-     "Page 4" : kalkulatorsegitiga
+     "Page 4" : kalkulatorsegitiga,
+     "Konversi foto" : main  
     }   
 st.sidebar.image("divino.eldadson.jpeg", width = 300)
 page = st.sidebar.radio("Halaman", list(PAGES.keys()))
